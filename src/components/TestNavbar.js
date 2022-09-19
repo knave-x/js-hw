5d0b985815156f50a7c46e84a5b74e0e6c3f97cc
knave-x <59017636+knave-x@users.noreply.github.com>
2022-09-19 12:03:37 +0300

    ++++

--- src/components/TestNavbar.js
+++ src/components/TestNavbar.js
@@ -1,56 +1,89 @@
-import React from 'react'
-import "../components/TestNavbar.css"
-import Container from 'react-bootstrap/Container';
-import Nav from 'react-bootstrap/Nav';
-import Navbar from 'react-bootstrap/Navbar';
-import NavDropdown from 'react-bootstrap/NavDropdown';
+import React from "react";
+import "../components/TestNavbar.css";
+import "/node_modules/flag-icons/css/flag-icons.min.css";
+import Container from "react-bootstrap/Container";
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Dropdown from 'react-bootstrap/Dropdown';
+import DropdownButton from 'react-bootstrap/DropdownButton';
+import {BsGlobe2} from "react-icons/bs"
 
 const TestNavbar = () => {
   return (
-    <div className='navbartest1'>
-    <Navbar className='navbartest2' collapseOnSelect expand="lg" bg="dark" variant="dark">
-      <Container  >
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#homepage">Anasayfa</Nav.Link>
-            
-            <NavDropdown title="Bugg Hakkında?" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Bugg Kimdir?</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">
-                Nasıl Çalışır
-              </NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.3">Bugg Partnerlik Başvurusu</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Yol Haritası
-              </NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#buggcommunity">Bugg Topluluğu</Nav.Link>
-            <Nav.Link href="#career">Kariyer</Nav.Link>
-            <Nav.Link href="#blog">Blog</Nav.Link>
-            <NavDropdown title="Destek?" id="collasible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Bize Ulaşın</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.2">
-                S.S.S.
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link href="#deets">More deets</Nav.Link>
-            <Nav.Link eventKey={2} href="#memes">
-              Dank memes
-            </Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
-    </div>
-  )
-}
+    <div className="navbartest1">
+      <Navbar
+        className="navbartest2"
+        collapseOnSelect
+        expand="lg"
+        bg="dark"
+        variant="dark"
+      >
+        <Container>
+          <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+          <Navbar.Collapse id="responsive-navbar-nav">
+            <Nav className="me-auto">
+              <Nav.Link href="#homepage">Anasayfa</Nav.Link>
+              
 
-export default TestNavbar
+              <NavDropdown title="Bugg Hakkında?" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                  Bugg Kimdir?
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">
+                  Nasıl Çalışır
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.3">
+                  Bugg Partnerlik Başvurusu
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Yol Haritası
+                </NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#buggcommunity">Bugg Topluluğu</Nav.Link>
+
+              <Nav.Link href="#career">Kariyer</Nav.Link>
+              <Nav.Link href="#blog">Blog</Nav.Link>
+              <NavDropdown title="Destek?" id="collasible-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">
+                <BsGlobe2/>
+                  Bize Ulaşın
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.2">S.S.S.</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+            <Nav>
+              <Nav.Link href="#deets">More deets</Nav.Link>
+              
+              <NavDropdown title="EN/TR"  id="collasible-nav-dropdown">
+                
+                <NavDropdown.Item href="#action/3.4">
+                    Türkçe 
+                <span class="fi fi-tr"></span>
+                
+               
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.5">
+                    English
+                    <span class="fi fi-us"></span>
+                </NavDropdown.Item>
+              </NavDropdown>
+              {/* </BsGlobe2>
+                <BsGlobe2 style={{fontSize:"50px"}}> */}
+              
+
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </div>
+  );
+};
+
+export default TestNavbar;
